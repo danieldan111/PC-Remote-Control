c30d3816e9b07d7afac353e17af0e3985007062c
danieldan111 <qpwnqpwn113@gmail.com>
2024-08-06 15:22:43 +0300

    dsd

---
+++ screen-share/streaming.py
@@ -0,0 +1,60 @@
+import socket
+import threading
+from PIL import ImageGrab #pip install pillow
+import io
+import time
+
+
+
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+
+screen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+screen.bind(ADDR)
+
+
+
+def screen_share(conn, addr):
+    sharing_screen = True
+    chunk_size = 1024
+
+    while sharing_screen:
+        img = ImageGrab.grab()
+
+        binary_stream = io.BytesIO()
+        img.save(binary_stream, format='PNG')
+        binary_data = binary_stream.getvalue()
+
+
+        img_size = str(len(binary_data)).encode()
+        img_size += b' ' * (100 - len(img_size))
+
+        conn.send(img_size)
+
+        total_sent = 0
+
+        total_sent = 0
+        while total_sent < len(binary_data):
+            chunk = binary_data[total_sent:total_sent + chunk_size]
+            if len(chunk) < 1024:
+                    chunk += b' ' * (1024 - len(chunk))
+
+            conn.send(chunk)
+            total_sent += 1024
+
+
+        # time.sleep()
+
+
+def screen_listen():
+    screen.listen()
+    print(f"[LISTENING] Server is listnening on {SERVER}")
+    while True:
+        conn, addr = screen.accept()
+        screen_share(conn, addr)
+        
+
+
+screen_listen()
---
+++ screen-share/watching.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+import time
+
+
+
+PORT = 5050
+SERVER = "10.0.0.21"  #the ip of the computer you want to control
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+
+screen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+screen.connect(ADDR)
+
+def screen_share():
+    watching_stream = True
+    while watching_stream:
+        img_size = int(screen.recv(100).decode(FORMAT))
+        # print(img_size)
+        if img_size:
+            
+            img_data = b''
+            while len(img_data) < img_size:
+                chunk = screen.recv(1024)
+                img_data += chunk
+            
+            with open("procces.png", 'wb') as screeny:
+                screeny.write(img_data)
+            
+            # time.sleep(0.1)
+            
+
+
+
+screen_share()
